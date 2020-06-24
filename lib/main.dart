dc32e1e4d1dd88505f30ad4977660baef4c4052e
abdullahwale <abdullahwale@gmai.com>
2020-06-24 14:24:38 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:planetxapp/ui/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Weight on Planet X',
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
